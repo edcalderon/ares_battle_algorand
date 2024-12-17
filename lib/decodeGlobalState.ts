6018ddc7fb0eb6a4b91d9b55e319489be85803af
edcalderon <edwardca12@hotmail.com>
2024-12-17 16:02:21 -0500

    updates

--- lib/decodeGlobalState.ts
+++ lib/decodeGlobalState.ts
@@ -10,7 +10,11 @@ export const decodeGlobalState = (globalState: Array<{ key: string; value: any }
             : (item.value.bytes ? Buffer.from(item.value.bytes, "base64").toString() : 'Invalid value');
 
         if (decodedKey === "g" && typeof item.value === 'object' && item.value.bytes) {
-                decodedValue = algosdk.encodeAddress(new Uint8Array(Buffer.from(item.value.bytes, "base64")));
+            decodedValue = algosdk.encodeAddress(new Uint8Array(Buffer.from(item.value.bytes, "base64")));
+        }
+
+        if (decodedKey === "hp") {
+            decodedValue = parseInt(item.value.uint)
         }
 
         return { key: decodedKey, value: decodedValue };
